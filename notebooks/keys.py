4f7c666585b7cb5369e98bccd10a6568a4006d31
PaulaEberitzsch <115081053+PaulaEberitzsch@users.noreply.github.com>
2022-11-19 14:51:25 +0100

    Create keys.py

---
+++ notebooks/keys.py
@@ -0,0 +1,19 @@
+# PYTHON FILE WITH TWITTER KEYS
+
+"""
+This file contains your Twitter keys
+
+    Replace the text with your keys
+    Keep the quotation marks
+
+"""
+
+consumer_key='lLIg4ly9DhjT97C41YuxPxhkg'
+
+consumer_secret='cvo5Az0vh5QaqbhzbbNMYsN8fhp11abalrkKjkkGQen434GOGl'
+
+access_token='1582677032240037888-79hznKgCpx6ozn1pYCE4MDfUl5nTUn'
+
+access_token_secret='xrloHz58tlYLLH0XT6t85XZEOzem2UzlwRzSU5H49BvZ3'
+
+bearer_token='AAAAAAAAAAAAAAAAAAAAAMJgiQEAAAAAtBeDR5o77XoHdvnAQ3gGKYh4N%2Fw%3DxekGchR9gzb7Jlrg0YyKdQxsEQ5qJMAmSEzVSJAHcEY44aqVm2'
