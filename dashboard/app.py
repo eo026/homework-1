f528eebbe88735ffb11cfd9332e4a22616b2d5ea
eo026 <94616073+eo026@users.noreply.github.com>
2022-12-20 20:57:16 +0100

    Update app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -54,15 +54,16 @@ st.write("Take a look at my data")
 st.dataframe(df)
 
 st.write("Take a look at my chart")
-# Make a chart with altair
-#Visualisierung 1
-c = alt.Chart(df).mark_circle().encode(
-     x='Gender', 
-     y='Race 1', 
-     color='Gender'
-     )
 
-chartRace = alt.Chart(df).mark_arc().encode(
+#Visualisierung 1
+
+st.subheader("Pie Chart")
+
+df['Gender'] = df['Gender'].astype("category")
+source = df[['Gender']]
+source = source.reset_index()
+
+chartRace = alt.Chart(source).mark_arc().encode(
     theta=alt.Theta(field="index", type="quantitative"),
     color= alt.Color ('Gender', 
                      legend=alt.Legend(title="Gender")),
@@ -84,36 +85,78 @@ pie = chartRace.mark_arc(outerRadius=70)
 pie
 
 
-# Bar chart
-st.subheader("Bar chart")
+# Visualisierung 2
+st.subheader("Horizontal Bar Chart")
 
-c = alt.Chart(df).mark_bar().encode(
-    x='Gender',
-    y='Race 1',
-    tooltip = ['Gender', "Race 1"]
+df['State'] = df['State'].astype("category")
+
+ChartState = alt.Chart(df).mark_bar().encode(
+    x=alt.X('count(State)',              
+            axis=alt.Axis(title = "Count")),
+    y=alt.X('State', 
+            axis=alt.Axis(title="State", 
+                          labelAngle=0)), 
+    color=alt.Color('State', scale=alt.Scale(scheme='set3')),
+    tooltip = ["count(State)","State"]
+
+).interactive(    
+
+).properties(title='Count of candidates in the states',
+             width=400,
+             height=800
+).configure_title(
+    fontSize=15,
+    font='Arial',
+    anchor='middle',
+    color='black'
+)
+ChartState
+
+# Visualisierung 3
+st.subheader("Normalized Stacked Bar Chart")
+
+df['State'] = df['State'].astype("category")
+df['Gender'] = df['Gender'].astype("category")
+
+alt.Chart(df).mark_bar().encode(
+    x=alt.X('count(Gender)', stack="normalize",
+        axis=alt.Axis(title = "Proportion of male and female candidates")),
+    y=alt.Y('State',
+        axis=alt.Axis(title = "State")),
+    color='Gender',
+    tooltip=["Gender", "count(Gender)", "State"]
+).interactive(
+).properties(title='Proportion of male and female candidates in the states',
+             width=400,
+             height=800
+).configure_title(
+    fontSize=15,
+    font='Arial',
+    anchor='middle',
+    color='black'
 )
 
-st.altair_chart(c, use_container_width=True)
+# Visualisierung 4
+st.subheader("Horizontal Stacked Bar Chart")
 
-# Show metric
-st.subheader("Display Metrics")
-st.metric("My metrics", 32, 4)
-
-# Add slider with user input
-st.subheader("Slider")
-st.write("This is a Slider")
-x = st.slider('x')  # 👈 this is a widget
-st.write(x, 'squared is', x * x)
-
-# Show metric
-st.subheader("Display Metrics")
-st.metric("My metrics", 32, 4)
-
-# Add slider with user input
-st.subheader("Slider")
-st.write("This is a Slider")
-x = st.slider('x')  # 👈 this is a widget
-st.write(x, 'squared is', x * x)
+df['Race 1'] = df['Race 1'].astype("category")
+alt.Chart(df).mark_bar().encode(
+    x=alt.X('count(Race 1)',
+        axis=alt.Axis(title = "Count")),
+    y=alt.Y('State',
+        axis=alt.Axis(title = "State")),
+    color='Race 1',
+    tooltip=["Race 1", "count(Race 1)", "State"]
+).interactive(
+).properties(title='Count of candidates in the states by race',
+             width=400,
+             height=800
+).configure_title(
+    fontSize=15,
+    font='Arial',
+    anchor='middle',
+    color='black'
+)
 
 ###-------------------###
 # END OF APP
